4ada284ff2294b4d1f33b965b549f608241729b4
Alpha Lencho <Alphalenchoo@gmail.com>
2024-10-11 21:47:40 +0300

    Add DBT setup and logging for data transformation and monitoring
    Installed DBT and initialized the project.
    Created DBT models for data transformation.
    Added DBT commands to run transformations and load data into the warehouse.
    Implemented DBT testing and documentation generation.
    Integrated logging to track scraping, capture errors, and monitor progress.

--- scripts/utils.py
+++ scripts/utils.py
@@ -14,3 +14,36 @@ def missing_values_table(df):
         "There are " + str(mis_val_table_ren_columns.shape[0]) +
           " columns that have missing values.")
     return mis_val_table_ren_columns
+
+def column_summary(df):
+    """Generate a summary of columns in the DataFrame."""
+    summary_data = []
+    
+    for col_name in df.columns:
+        col_dtype = df[col_name].dtype
+        num_of_nulls = df[col_name].isnull().sum()
+        num_of_non_nulls = df[col_name].notnull().sum()
+        num_of_distinct_values = df[col_name].nunique()
+        
+        if num_of_distinct_values <= 10:
+            distinct_values_counts = df[col_name].value_counts().to_dict()
+        else:
+            top_10_values_counts = df[col_name].value_counts().head(10).to_dict()
+            distinct_values_counts = {k: v for k, v in sorted(top_10_values_counts.items(), key=lambda item: item[1], reverse=True)}
+
+        summary_data.append({
+            'col_name': col_name,
+            'col_dtype': col_dtype,
+            'num_of_nulls': num_of_nulls,
+            'num_of_non_nulls': num_of_non_nulls,
+            'num_of_distinct_values': num_of_distinct_values,
+            'distinct_values_counts': distinct_values_counts
+        })
+    
+    summary_df = pd.DataFrame(summary_data)
+    return summary_df
+
+
+
+
+
